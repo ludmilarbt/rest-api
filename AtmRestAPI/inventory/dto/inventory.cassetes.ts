9f4dcca7bca1096128428d2596c1e759782e60b9
ludmilarbt <34265584+ludmilarbt@users.noreply.github.com>
2022-05-22 17:03:58 +0300

    Prepare calculation
    Prepare calculation

---
+++ AtmRestAPI/inventory/dto/inventory.cassetes.ts
@@ -0,0 +1,37 @@
+export const inventoryBalance=[
+    {
+        "_id": "6289f0b6c521fb001b33f665",
+        "type": "Bill",
+        "value": 100,
+        "amount": 2,
+        "__v": 0
+    },
+    {
+        "_id": "6289f1ccf8b4bc001aef6a7b",
+        "type": "Bill",
+        "value": 100,
+        "amount": 2,
+        "__v": 0
+    },
+    {
+        "_id": "6289f2c8d3f427001aebc17a",
+        "type": "Bill",
+        "value": 100,
+        "amount": 2,
+        "__v": 0
+    },
+    {
+        "_id": "6289f6f6d712548e70049eef",
+        "type": "Bill",
+        "value": 100,
+        "amount": 2,
+        "__v": 0
+    },
+    {
+        "_id": "6289f80088e7689b90aa8008",
+        "type": "Bill",
+        "value": 100,
+        "amount": 2,
+        "__v": 0
+    }
+]
--- AtmRestAPI/operations/services/withdrawal.service.ts
+++ AtmRestAPI/operations/services/withdrawal.service.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import withdrawalDao from '../daos/withdrawal.dao';
 import {AtmCashResponse} from '../../inventory/dto/atm.cash.response'
+import {inventoryBalance} from '../../inventory/dto/inventory.cassetes';
 
 
 class WithdrawalService {
     async withdrawMoney(amount: number) {
         const atmCashResponse= await withdrawalDao.withdrawMoney(amount);
 
+
+
         //if response is valid send approval
         return atmCashResponse;
     }
 
     async listInventoryItems() {
+        console.log('inventoryBalance' + JSON.stringify(inventoryBalance));
+
+        
+        
         const inventory= await withdrawalDao.getInventory();
 
         //if response is valid send approval
